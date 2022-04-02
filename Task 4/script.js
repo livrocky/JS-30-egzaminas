a18c0b4dcaf5732113ad16d83c57fcae9c944504
rokas <rokasra@gmail.com>
2022-04-02 19:47:01 +0300

    task4 - js fetch done, innerHTML done

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,30 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+
+const outputEl = document.getElementById("output");
+
+async function getCars() {
+  const resp = await fetch(ENDPOINT);
+  const data = await resp.json();
+  renderCars(data, outputEl);
+}
+getCars();
+
+function renderCars(carsArr, dest) {
+  dest.innerHTML = "";
+  carsArr.forEach((cObj) => {
+    dest.append(showCars(cObj));
+  });
+}
+function showCars(carObj) {
+  const divEl = document.createElement("div");
+  console.log("divEl===", divEl);
+  divEl.className = "cars-card";
+  divEl.innerHTML = `
+    <h3 class="brand">${carObj.brand}</h3>
+    <p class="model">${carObj.models.join(", ")}.</p>
+    `;
+  return divEl;
+}
