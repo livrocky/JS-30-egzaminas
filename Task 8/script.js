079a3e05a87fd74588465da3f52c911e1386a9d8
rokas <rokasra@gmail.com>
2022-04-03 14:34:31 +0300

    Tsk 8- value returned together, need fix

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,52 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(num1, num2) {
+  this.num1 = num1;
+  this.num2 = num2;
+
+  this.sum = function () {
+    return this.num1 + this.num2;
+  };
+  this.subtraction = function () {
+    return this.num1 - this.num2;
+  };
+  this.multiplication = function () {
+    return this.num1 * this.num2;
+  };
+  this.division = function () {
+    return this.num1 / this.num2;
+  };
+}
+
+const calc = new Calculator(2, 2);
+console.log("Sum===", calc.sum());
+console.log("Subtraction===", calc.subtraction());
+console.log("Multiplication===", calc.multiplication());
+console.log("Division===", calc.division());
+
+// function Calculator(num1, num2) {
+//   this.sum = function (num1, num2) {
+//     num1 + num2;
+//   };
+//   //   this.subtraction = num1 - num2;
+//   //   this.multiplication = num1 * num2;
+//   //   this.division = num1 / num2;
+// }
+// const calc = new Calculator(5, 15);
+// console.log(calc);
+
+// function Calculator(num1, num2) {
+//   this.num1 = num1;
+//   this.num2 = num2;
+//   this.sum = function () {
+//     return this.num1 + this.num2;
+//     //     this.subtraction = num1 - num2;
+//     // this.multiplication = num1 * num2;
+//     // this.division = num1 / num2;
+//   };
+// }
+
+// this.sum = new Calculator(2, 5);
+// console.log("calc===", new Calculator(2, 2));
